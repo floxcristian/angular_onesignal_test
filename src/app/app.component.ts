847a31cb34db0c3e479db6deaf948eadad0cedc5
floxcristian <cristian.flores@alumnos.uta.cl>
2020-06-03 22:04:11 -0400

    add onesignal sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-push-notification';
+
+  constructor(
+    //private _swPush: SwPush
+  ){}
+
+  /*
+  subscribeToNotifications(){
+    this._swPush.requestSubscription({
+      serverPublicKey: 
+    })
+  }*/
+
+  sendMeANotification(){
+    
+  }
 }
